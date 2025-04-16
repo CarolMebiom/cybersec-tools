c60ac5753a32d950eecc938fcf36b0bf5930ca38
CarolMebiom <59604360+CarolMebiom@users.noreply.github.com>
2025-04-16 17:42:12 +0100

    Update host-disco.py

--- host-disco.py
+++ host-disco.py
@@ -1,13 +1,15 @@
 import socket
 
-def send_arp():
-    sock = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(3))
+def discover_host(ip):
+    sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDPLITE)
+    sock.timeout(0.5)
+    port = 40555
+    port_sock = 30555
     try:
-        sock.bind(("eth0",0))
-        sock.send(packet)
-        print(sock.recv(1024))
+        sock.sendto("ping".encode(),(ip,port))
+        sock.bind(ip, port_sock)
+        received = sock.recvfrom(1024)
+        print(received)
+    
     except Exception as e:
         print(e)
-
-if __name__ == "__main__":
-    send_arp()
